bf8e3de7cb50b3ba6399f3454de8a123eaa915c3
Hector Sabillon <hdannysabillon992232@gmail.com>
2023-05-05 14:14:02 -0600

    osi2

--- src/lab3p2_hectorsabillon/Lab3P2_HectorSabillon.java
+++ src/lab3p2_hectorsabillon/Lab3P2_HectorSabillon.java
@@ -24,16 +24,35 @@ public class Lab3P2_HectorSabillon {
             sc.nextLine();
             switch (opcion) {
                 case 1: {
-
+                    agregarComida();
                     break;
                 }
                 case 2: {
+                    agregarBebida();
                     break;
                 }
                 case 3: {
                     break;
                 }
                 case 4: {
+                    System.out.println("---MODIFICAR---"
+                            + "\n1 <- Comida"
+                            + "\n2 <- Bebida");
+                    int op = sc.nextInt();
+                    sc.nextLine();
+                    switch (op) {
+                        case 1: {
+                            modComida();
+                            break;
+                        }
+                        case 2: {
+                            
+                            break;
+                        }
+                        default:
+                            System.out.println("Opcion no valida");
+                            break;
+                    }
                     break;
                 }
                 case 5: {
@@ -66,32 +85,124 @@ public class Lab3P2_HectorSabillon {
             case 0: {
                 estado = false;
                 Comidas comida = new Comidas(estado, precio, nombre);
-                lista.add(comida);
                 break;
             }
             case 1: {
                 estado = true;
                 Comidas comida = new Comidas(estado, precio, nombre);
-                lista.add(comida);
                 break;
             }
             default:
                 System.out.println("Opcion no valida");
                 break;
         }
-
     }
-    
-    public static void agregarBebida(){
+
+    public static void agregarBebida() {
         System.out.println("Ingrese nombre de la bebida: ");
         String nombre = sc.nextLine();
         System.out.println("Ingrese precio: ");
         double precio = sc.nextDouble();
         System.out.println("Ingrese tam en mL: ");
         int tam = sc.nextInt();
-        
+
         Bebidas bebida = new Bebidas(tam, precio, nombre);
         lista.add(bebida);
     }
 
+    public static void listarComidas() {
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i) instanceof Comidas) {
+                System.out.println(i + " <- " + lista.get(i));
+            }
+        }
+    }
+
+    public static void listarBebidas(){
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i) instanceof Bebidas) {
+                System.out.println(i + " <- " + lista.get(i));
+            }
+        }
+    }
+    
+    public static void modComida() {
+        listarComidas();
+        System.out.println("Ingrese indice de la comida a modificar: ");
+        int index = sc.nextInt();
+        sc.nextLine();
+        System.out.println("---ASPECTOS A MODIFICAR---"
+                + "\n1 <- Nombre"
+                + "\n2 <- Precio"
+                + "\n3 <- Estado");
+        int aspecto = sc.nextInt();
+        switch (aspecto) {
+            case 1: {
+                System.out.println("Ingrese nombre de la comida: ");
+                String nombre = sc.nextLine();
+                ((Comidas) lista.get(index)).setNombre(nombre);
+                break;
+            }
+            case 2: {
+                System.out.println("Ingrese precio: ");
+                double precio = sc.nextDouble();
+                ((Comidas) lista.get(index)).setPrecio(precio);
+                break;
+            }
+            case 3: {
+                System.out.println("Ingrese estado de la comida:"
+                        + "\n[0] <- Vencido"
+                        + "\n[1] <- Comestible");
+                int a = sc.nextInt();
+                boolean estado;
+                switch (a) {
+                    case 0: {
+                        estado = false;
+                        ((Comidas) lista.get(index)).setEstado(estado);;
+                        break;
+                    }
+                    case 1: {
+                        estado = true;
+                        ((Comidas) lista.get(index)).setEstado(estado);
+                        break;
+                    }
+                    default:
+                        System.out.println("Opcion no valida");
+                        break;
+                }
+                break;
+            }
+            default:
+                System.out.println("Opcion no valida");
+                break;
+        }
+        
+    }
+
+    public static void modBebida(){
+        listarBebidas();
+        System.out.println("Ingrese indice de la bebida a modificar: ");
+        int index = sc.nextInt();
+        sc.nextLine();
+        System.out.println("---ASPECTOS A MODIFICAR---"
+                + "\n1 <- Nombre"
+                + "\n2 <- Precio"
+                + "\n3 <- Tamaño");
+        int aspecto = sc.nextInt();
+        switch(aspecto){
+            case 1:{
+                break;
+            }
+            case 2:{
+                break;
+            }
+            case 3:{
+                break;
+            }
+            default:
+                System.out.println("Opcion no valida");
+                break;
+        }
+    }
+    
 }
